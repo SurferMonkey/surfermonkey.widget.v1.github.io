3d18f41bd08361b9251873ee311f9c6a233fd525
miguelCyclone <miguelhongo90@gmail.com>
2023-10-25 15:13:20 +0530

    fix landing

--- src/index.js
+++ src/index.js
@@ -1,60 +1,62 @@
-import React, { useState } from 'react';
-import DepositERC20 from './components/DepositERC20';
-import DepositNative from './components/DepositNative';
-import Withdraw from './components/Withdraw';
-import DeveloperLogs from './components/DeveloperLogs';
-import SurferMonkeyLogo from "./Images/SurferMonkey.png";
-import TagLine from "./Images/TagLine.png";
-import './SurferMonkey.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import SurferMonkey from './SurferMonkey';
+import * as configWidget from './configWidget';
 
-function SurferMonkey({ userMessage }) {
-  const [activeView, setActiveView] = useState('start');
-  const [showDevLogs, setShowDevLogs] = useState(false); // State to track visibility of DeveloperLogs
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-  // Decide which component to render based on userMessage.selectedFunc
-  const renderDepositComponent = () => {
-    switch (userMessage.selectedFunc) {
-      case 1:
-        return <DepositNative userMessage={userMessage} />;
-      case 2:
-        return <DepositERC20 userMessage={userMessage} />;
-      default:
-        return null;
-    }
-  };
+// ***********************************************
+// Developer: START Capture data from the user
+// ***********************************************
 
-  return (
-    <div className="app-container">
-      <div className="menu">
-        <button
-          onClick={() => setActiveView('start')}
-          className={activeView === 'start' ? 'selected' : ''}
-        >
-          Start
-        </button>
-        <button
-          onClick={() => setActiveView('withdraw')}
-          className={activeView === 'withdraw' ? 'selected' : ''}
-        >
-          Finish
-        </button>
-      </div>
-      {activeView === 'start' && renderDepositComponent()}
-      {activeView === 'withdraw' && <Withdraw />}
-      {/* Footer */}
-      <footer className="app-footer">
-        <a href="https://www.surfermonkey.io/" target="_blank" rel="noopener noreferrer">
-          <img src= {SurferMonkeyLogo} alt="SurferMonkey" className="footer-logo" />
-        </a>
-      </footer>
-      {/* Button to toggle DeveloperLogs */}
-      <button className="dev-logs-button" onClick={() => setShowDevLogs(!showDevLogs)}>
-        Dev Logs
-      </button>
-      {/* Conditionally render DeveloperLogs based on showDevLogs */}
-      {showDevLogs && <DeveloperLogs userMessage={userMessage} />}
-    </div>
-  );
-}
+const ERC20_SC = "" //  Only used for ERC20, blank string if not used. Hex String
+const targetBlockchainString = "" // Check config file for supported blockchains. String
+const sourceBlockchainString = ""
+const selectedFunc = 1// (Native: nativeFunc = 1) OR (ERC20: erc20Func = 2). Integer
+const functionHeader = "" // The function header from the target function. String
+const functionName = "" // The function name from the target function. String
+const payloadParmsArr = [] // The target function parameters. Array of strings
+const amount = 1 // The amount of value of ERC20 or Native to be locked in the System. UINT256
+const targetSC = "" // The target Smart Contract Address. Hex String
+const userEOA = "" // the User EOA. Hex String
 
-export default SurferMonkey;
+// ***********************************************
+// Developer: END Capture data
+// ***********************************************
+
+
+// ----------------------------------------------
+
+
+// ***********************************************
+// Developer: START DO NOT MODIFY - DO NOT MODIFY
+// ***********************************************
+const UniversalPluginGlobalHash = configWidget.UP_V1 // Universal Plugin Version 1
+const userMessage = configWidget.createUserMessage(
+  selectedFunc,
+  targetBlockchainString,
+  sourceBlockchainString,
+  ERC20_SC,
+  amount,
+  functionHeader,
+  functionName,
+  payloadParmsArr,
+  targetSC,
+  userEOA,
+  UniversalPluginGlobalHash
+)
+
+// ***********************************************
+// Developer: END DO NOT MODIFY - DO NOT MODIFY
+// ***********************************************
+
+
+// ----------------------------------------------
+
+// Developer: Send to SurferMonkey Widget (React Component), the "userMessage"
+root.render(
+  <React.StrictMode>
+    <SurferMonkey userMessage={userMessage} />
+  </React.StrictMode>
+);
